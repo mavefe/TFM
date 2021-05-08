e3f78ed45ec934cc61a51c257f5f6f8de06326a2
mavefe <dc1996mariavelafelardo@gmail.com>
2021-05-08 18:33:07 +0200

    Commit3

--- app.R
+++ app.R
@@ -1,23 +1,19 @@
 library(shiny)
 ui <- fluidPage(
-  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
-  verbatimTextOutput("summary"),
-  tableOutput("table")
+  textInput("name", "What's your name?"),
+  textOutput("greeting")
 )
+
 server <- function(input, output, session) {
-  # Create a reactive expression
-  dataset <- reactive({
-    get(input$dataset, "package:datasets")
-  })
-  
-  output$summary <- renderPrint({
-    # Use a reactive expression by calling it like a function
-    summary(dataset())
-  })
-  
-  output$table <- renderTable({
-    dataset()
+  output$greeting <- renderText({
+    paste0("Hello ", input$name, "!")
   })
 }
 shinyApp(ui, server)#ctrl+shift+enter atajo para abrir
 #para cerrar la app pulsar esc
+
+#para que se muestre en el viewer (pero primero, en el shiny-run.R,
+#hay que darle a Jobs/Start local job y poner esa URL):
+#rstudioapi::viewer("http://127.0.0.1:3582")
+#Haciendo esto, cada vez que guarde (con Ctrl+S por ejemplo)
+#se actualiza el viewer.
