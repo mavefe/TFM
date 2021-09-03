83c9bdd163206205e4a35dcf30d182e7358961ee
mavefe <dc1996mariavelafelardo@gmail.com>
2021-09-03 11:36:23 +0200

    prueba 03092021

--- app.R
+++ app.R
@@ -1,15 +1,10 @@
-# importamos datos
-cv_vacuna = read.csv("https://github.com/owid/covid-19-data/blob/master/public/data/vaccinations/vaccinations.csv")
-cv_tipos = read.csv("https://github.com/owid/covid-19-data/blob/master/public/data/vaccinations/vaccinations-by-manufacturer.csv")
+# App básica
 
-head(cv_tipos)
+library(shiny)
+ui <- fluidPage("Hola mundo")
 
-# rellenamos datos faltantes
-for(i in 1:ncol(cv_vacuna))
-  for (j in 1:nrow(cv_vacuna))
-    if (is.null(cv_vacuna[i,j]) & (j=0)) {
-      cv_vacuna[i,j]=0
-    }
-    if (is.null(cv_vacuna[i,j]) & j!=0) {
-      cv_vacuna[i,j]=cv_vacuna[i,j-1]
-    }
+server <- function(input, output) {}
+
+shinyApp(ui = ui, server = server)
+
+# Fin app básica
