87be0cb0719d7ecf4a321880b265336d79eab045
mavefe <dc1996mariavelafelardo@gmail.com>
2021-10-10 11:15:48 +0200

    10102021_1115

--- app.R
+++ app.R
@@ -173,7 +173,7 @@ server = function(input, output) {
   observeEvent(input$pauta,    {variable$data <- "people_fully_vaccinated"}) # se pone la columna
   observeEvent(input$vac_dia,  {variable$data <- "daily_vaccinations"})      # correspondiente
   
-  vacunas_pais_variable_no_vacía <- reactive({          # Filtro el dataset para que elimine
+  vacunas_pais_variable_no_vacia <- reactive({          # Filtro el dataset para que elimine
     filter(vacunas_pais, is.na(variable$data) == FALSE) # los registros cuyo valor
   })                                                    # en la variable elegida no esté vacío
   
